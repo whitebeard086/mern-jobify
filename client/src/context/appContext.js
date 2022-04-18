a8cdd3784a0737f3a067d7c9f8df436f64fe29f7
unknown <whitebeard086@gmail.com>
2022-04-18 09:14:10 +0100

    setup global context and reducer

---
+++ client/src/context/appContext.js
@@ -0,0 +1,24 @@
+import React, { useReducer, useContext } from "react";
+import reducer from "./reducers";
+
+
+const initialState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: "",
+  alertType: "",
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>;
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, initialState, useAppContext };
