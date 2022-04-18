c734386c5b8268cf35eaf1822f4f65c7371d8ccc
unknown <whitebeard086@gmail.com>
2022-04-18 17:15:54 +0100

    auth and jobs controllers and routes setup

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
-import connectDB from "./db/connect.js";
 dotenv.config();
+
+// db and authenticate user
+import connectDB from "./db/connect.js";
+
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
