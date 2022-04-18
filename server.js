a80e0bd6a55b0437a1e1f7aa41dfc25cb8b8d949
unknown <whitebeard086@gmail.com>
2022-04-18 16:19:51 +0100

    basic server setup and connect DB

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+dotenv.config();
+
+// middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
