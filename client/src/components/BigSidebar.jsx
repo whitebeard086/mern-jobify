1117ef6c765d3dbac53926fbf13417a848718ce1
unknown <whitebeard086@gmail.com>
2022-04-20 19:13:13 +0100

    shared layout structure

---
+++ client/src/components/BigSidebar.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "../assets/wrappers/BigSidebar";
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <h1>Big sidebar</h1>
+    </Wrapper>
+  );
+};
+export default Navbar;
---
+++ client/src/components/SmallSidebar.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "../assets/wrappers/SmallSidebar";
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <h1>Small sidebar</h1>
+    </Wrapper>
+  );
+};
+export default Navbar;
