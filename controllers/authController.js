0feb7954290935e1119fc45677d321af3c738698
unknown <whitebeard086@gmail.com>
2022-04-21 09:56:26 +0100

    update user - server

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,8 +54,24 @@ const login = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  res.send("update user");
-  console.log(req.user);
+  const { name, email, lastName, location } = req.body;
+
+  if (!name || !email || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.name = name;
+  user.email = email;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });  
 };
 
 export { register, login, updateUser };
