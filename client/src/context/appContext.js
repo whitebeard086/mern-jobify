2f4996ae2684dcc2ef01d9bd707f08bc5701f9e2
unknown <whitebeard086@gmail.com>
2022-04-21 10:37:28 +0100

    profile page

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -82,8 +82,12 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage();
   };
 
+  const updateUser = async (currentUser) => {
+    console.log(currentUser);
+  }
+
   return (
-    <AppContext.Provider value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser }}>
+    <AppContext.Provider value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser, updateUser }}>
       {children}
     </AppContext.Provider>
   );
